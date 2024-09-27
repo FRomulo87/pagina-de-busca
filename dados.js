26b4d2008d6af16a131518e0b6c05290fecf3f57
FRomulo87 <76665843+FRomulo87@users.noreply.github.com>
2024-09-26 21:12:11 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+
+  
+let dados = [
+   {
+     titulo: "Rayssa Leal",
+     descricao: "Rayssa Leal, conhecida como 'Fadinha', é uma skatista brasileira que fez história ao conquistar a medalha de prata nos Jogos Olímpicos de Tóquio 2020. Com apenas 13 anos, ela se destacou no skate mundial, inspirando uma nova geração de atletas.",
+     link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+   },
+   {
+     titulo: "Rebeca Andrade",
+     descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional, vencendo medalhas nos Jogos Olímpicos de Tóquio 2020.",
+     link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+   },
+   {
+     titulo: "Beatriz Souza",
+     descricao: "Beatriz Souza é uma judoca brasileira que se destaca na categoria peso-pesado. Ela é uma das principais atletas do judô no Brasil, com diversas conquistas em campeonatos mundiais e pan-americanos, representando o país em competições de alto nível.",
+     link: "https://pt.wikipedia.org/wiki/Beatriz_Souza_(judoca)"
+   }
+];
+
+
+
+
